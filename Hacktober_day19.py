e15a1522d1aa1f75e246d9d885c0966d47335475
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-10-19 19:53:05 +0530

    Add files via upload

---
+++ Hacktober_day19.py
@@ -0,0 +1,20 @@
+
+def count():
+    from random import randint
+    n=10
+    if n<=100:    
+        count=0
+        matrix = [[]*n for i in range(n)]
+        for o in range(n):
+            for k in range(n):
+                ran=randint(1, 5)
+                matrix[o].append(ran)
+        for i in matrix:            
+            print(i)
+        count =0
+        for i in matrix:                
+                for j in range(len(i)) :                    
+                    if (i[j])==5:
+                        count+=1
+        print("count's' of 5:",count)
+count()
