0243fd156dffaf04d3f13c1934281271697ecc4d
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-10-22 18:25:42 +0530

    return index differecnce  between  two str

--- Hacktober_day22.py
+++ Hacktober_day22.py
@@ -5,7 +5,7 @@ def func_main(s,t,n,m):
     if s[n]==t[m]:
         n+=1
         m+=1
-    if (n>len(s)-1 or m>len(s)-1):
+    if (n>len(s)-1):
         return m
     if m<=len(t):    
         return func_main(s,t,n,m)           
