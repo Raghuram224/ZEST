30a52f791ce63947b474b45e90c658a945b264f3
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-10-28 18:25:30 +0530

    multipliaction table in modulo

--- Hacktober_day27.py
+++ Hacktober_day27.py
@@ -1,19 +1,8 @@
-input1,v=int(input("enter:")),[]
-for i in range(1,input1+1):
-    v.append(i)
+def modulo(n):
+    for i in range(1,n):
+        for  j in range(1,n):
+            k=i*j%n
+            print(k,end=' ')
+        print()
 
-n=v*input1
-print(n)
-print(n[8])
-for r in range(1,input1):
-    for c in range(1,input1):
-        x=r*c
-        # print(x)
-        if x<input1:
-            e=''
-        else:
-            if x<input1*10:
-                e=''
-        print(e,n[x-1],end='')
-    # print(x)
-    print()
+modulo(n=int(input('Enter n:')))
