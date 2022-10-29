d5ea75dfd12fc8e1960dc5982c4c227bb5700b64
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-10-29 07:41:26 +0530

    recursion boolean list 1 and 2 have same elements in different order return true

---
+++ Hacktober_day29.py
@@ -0,0 +1,17 @@
+
+# input from same line with separate space like 1 1 2 3
+def sameElements(l1,l2,i):    
+    if len(l1) == len(l2)>0:
+        l1.sort()
+        l2.sort()
+        if i==len(l1)-1:
+            return True
+        if l1[i]==l2[i]:
+            i+=1
+        else:
+            return False   
+    else:
+        return False 
+    return sameElements(l1, l2, i)
+
+print(sameElements(l1=[int(x) for x in input('Enter list 1:').split()],l2=[int(x) for x in input('Enter list 2:').split()],i=0))
