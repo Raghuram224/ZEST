a69d82f93ceff2f1bcc6c7ecbff5c96040883ad2
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-11-13 08:31:11 +0530

    rangoli pattern(important)

--- Hacktober_day15.py
+++ Hacktober_day15.py
@@ -1,4 +1,20 @@
+# like pyramid pattern it will easy ensure reverse and add in doube (())
+# import string
+# def rangoli(n):
+#     l=[]
+#     st=string.ascii_lowercase
+#     print(st)
+#     for i in range(n):
+#         s='-'.join(st[i:n])
+#         l.append((s[::-1]+s[1:]).center(4*n-3,'-'))    
+#     j=1
+#     for i in range(n):
+#         print(l[n-j])
+#         j+=1
+#     for i in range(n-1):
+#         print(l[i+1]) 
 
+# rangoli(3)
 def rangoli_alpha(n):
     al = list(map(chr,range(97,123)))    
     x=al[n-1::-1]+al[1:n]   
