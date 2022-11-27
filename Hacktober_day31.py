934e4ee22b94a04dc4e19e1afa89bd40f15e20e5
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-11-27 20:27:11 +0530

    another code add in minsweeper

--- Hacktober_day31.py
+++ Hacktober_day31.py
@@ -1,4 +1,58 @@
+   #another code
 
+def minesweeper(n,m):
+    import random
+    ran=[0,'M']
+    array = [[0 for column in range(m)] for row in range(n)]
+    for i in range(n):
+        for j in range(m):
+            array[i][j]=random.choice(ran)
+
+    for a in array:
+        for val in a:
+            print(val,end=' ')
+        print()
+    arr=array
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j]!="M":
+                temp=0
+                if i+1<n:
+                    if arr[i+1][j]=='M':#bottom
+                        temp+=1
+                if j+1<m:               #right
+                    if arr[i][j+1]=="M":
+                        temp+=1
+                if (i-1)>-1:
+                    if arr[i-1][j]=='M':#top
+                        temp+=1
+                if (j-1)>-1:
+                    if arr[i][j-1]=='M':#left
+                        temp+=1
+                if i+1<n and j+1<m: #right bottom
+                    if arr[i+1][j+1]=='M':
+                        temp+=1
+                if (i-1)>-1 and (j-1)>-1:
+                    if arr[i-1][j-1]=='M': #upper left
+                        temp+=1
+                if i+1<n and (j-1)>-1:
+                    if arr[i+1][j-1]=='M':#left bottom
+                        temp+=1
+                if (i-1)>-1 and j+1<m: #upper right
+                    if arr[i-1][j+1]=='M':
+                        temp+=1
+                arr[i][j]=str(temp)
+    print('\n Result \n')
+    for ar in array:
+            for val in ar:
+                print(val,end=' ')
+            print()
+
+
+
+minesweeper(5, 4)
+
+#old code
 def minesweeper(n,m):
     import random
     ran=[0,'M']
@@ -48,3 +102,7 @@ def minesweeper(n,m):
 
 if __name__=='__main__':
     minesweeper(n= int(input("Enter n:")), m=int(input("Enter m:")))
+    
+    
+    
+ 
