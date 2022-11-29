2024bad7f2580fc40989f5bdc272ec4a9d36740d
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-11-29 15:06:19 +0530

    recursion add 1 to every element in list

--- Hacktober_day13.py
+++ Hacktober_day13.py
@@ -1,4 +1,14 @@
 
+#another code
+def fun(num,val):
+    if val<len(num):
+        num[val]+=1
+        val+=1
+        return fun(num, val)
+    return num
+
+
+print(fun(num=[2,6,3,8], val=0))
 
 def add_one_to_everything(a=[]):
     c=[]
