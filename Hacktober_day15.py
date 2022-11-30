ace89c6eb210fa9023c8196e502f504c1a3bf0c9
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2022-11-30 14:50:12 +0530

    rangoli

--- Hacktober_day15.py
+++ Hacktober_day15.py
@@ -15,16 +15,17 @@ def rangoli(n):
         print(l[i+1])     
 
 rangoli(3)
-
-
+#old code
 def rangoli_alpha(n):
     al = list(map(chr,range(97,123)))    
-    x=al[n-1::-1]+al[1:n]   
-    width=len('-'.join(x))
+    x=al[n-1::-1]+al[1:n]   #reverse la varum c b a + b c n-1::-1 reverse la value c b a
+    
+    width=len('-'.join(x)) #width c-b-a-b-c total 9
     for i in range(1,n):
-        print('-'.join(al[n-1:n-i:-1]+al[n-i:n]).center(width,'-'))
-    for j in range(n,0,-1):
-        print('-'.join(al[n-1:n-j:-1]+al[n-j:n]).center(width,'-'))
+        print('-'.join(al[n-1:n-i:-1]+al[n-i:n]).center(width,'-')) #the first part remains empt and 
+        #second part n-0:n c and n-1:n == b c
+    for j in range(n,0,-1): # reverse 3 2 1start stop increment/ step
+        print('-'.join(al[n-1:n-j:-1]+al[n-j:n]).center(width,'-')) #j will be 3 and 2:0:-1 +0:3 c b +a b c
     
 n=3
 rangoli_alpha(n)
